5b16e1feb0c6f252ff80bc9fb349eba6992a9d2b
Heeba Merchant <heebamerchant10@gmail.com>
2025-02-22 12:10:17 -0500

    change

--- frontend/src/login.tsx
+++ frontend/src/login.tsx
@@ -2,9 +2,21 @@ import { Link } from "react-router";
 
 export function Login() {
 	return (
-		<div>
-			<Link to="/">Go to home!</Link>
-			Hello world, this is login!
-		</div>
+		<>
+			{/* Login Container */}
+			<div className="flex flex-col gap-4 max-w-md mx-auto w-full p-4">
+				<div>
+					{/* Header */}
+					<Link to="/"> Go to home! </Link>
+					Hello world, this is login!
+				</div>
+				{/* Login Form */}
+				<div className="flex flex-col gap-3">
+					<input type="text" placeholder="Username" className="max-w-xs" />
+					<input type="password" placeholder="Password" className="max-w-xs" />
+					<button className="max-w-xs">Login</button>
+				</div>
+			</div>
+		</>
 	);
 }
