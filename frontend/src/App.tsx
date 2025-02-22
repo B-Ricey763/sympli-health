be072fd8f78b6bd0079aaaa8b0e86d52192c2b36
B-Ricey763 <bhanna30@gatech.edu>
2025-02-22 10:16:50 -0500

    routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,22 @@
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
+import { Link } from "react-router";
 
 function App() {
 	const [count, setCount] = useState(0);
 
+	async function getHealth() {
+		const res = await fetch(
+			"https://index-router-57685090875.us-east1.run.app/health",
+		);
+		console.log(await res.json());
+	}
+
 	return (
 		<>
-			<Button>Hello world</Button>
+			<Button asChild>
+				<Link to="/login">Login</Link>
+			</Button>
 		</>
 	);
 }
---
+++ frontend/src/login.tsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router";
+
+export function Login() {
+	return (
+		<div>
+			<Link to="/">Go to home!</Link>
+			Hello world, this is login!
+		</div>
+	);
+}
